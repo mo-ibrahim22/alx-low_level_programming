2fb10f659ca9f8090c2fa89741d6eabbda746b66
mo-ibrahim22 <mibrahimm002@gmail.com>
2023-03-22 17:30:46 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,10 @@
 #include "main.h"
-#include <unistd.h>
-int _putchar(char c)
+int main(void)
 {
-	return (write(1,&c,1);
-}			
+	char str[] = "_putchar";
+int ch;
+for (ch=0; ch<8; ch++)
+	_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}
